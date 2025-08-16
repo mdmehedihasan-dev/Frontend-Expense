ed361b7dd44b4c45a303799136f726c0bd9582c9
mdmehedihasan-dev <mdmehedihasandev0@gmail.com>
2025-08-16 16:17:36 +0600

    chart component

---
+++ src/components/ExpenseChart.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { PieChart, Pie, Cell, ResponsiveContainer, Tooltip, Legend } from 'recharts';
+
+const COLORS = ['#8884d8', '#82ca9d', '#ffc658', '#ff8042', '#00c49f', '#ffc658', '#a4de6c'];
+
+const ExpenseChart = ({ expenses }) => {
+  const data = expenses.reduce((acc, expense) => {
+    const existingCategory = acc.find(item => item.name === expense.category);
+    if (existingCategory) {
+      existingCategory.value += expense.amount;
+    } else {
+      acc.push({ name: expense.category, value: expense.amount });
+    }
+    return acc;
+  }, []);
+
+  return (
+    <div >
+      <ResponsiveContainer width="100%" height={280}>
+        <PieChart>
+          <Pie
+            data={data}
+            cx="50%"
+            cy="50%"
+            labelLine={false}
+            outerRadius={100}
+            fill="#8884d8"
+            dataKey="value"
+          >
+            {data.map((entry, index) => (
+              <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+            ))}
+          </Pie>
+          <Tooltip />
+          <Legend />
+        </PieChart>
+      </ResponsiveContainer>
+    </div>
+  );
+};
+
+export default ExpenseChart;
